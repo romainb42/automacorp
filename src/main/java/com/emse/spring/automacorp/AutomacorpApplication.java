9fa881f6a1ede1fdb2cb74ab2b90dafa3786984e
CelineNi2 <celine.ni@etu.emse.fr>
2024-01-09 18:21:12 +0100

    Testing the automatization of unit testing

--- src/main/java/com/emse/spring/automacorp/AutomacorpApplication.java
+++ src/main/java/com/emse/spring/automacorp/AutomacorpApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AutomacorpApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AutomacorpApplication.class, args);
 	}
